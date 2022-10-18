65d56c88bf39e9c7d386f2e6ef478c7a47ecad9e
Ofentse Loeto <ohentseloeto610@gmail.com>
2022-10-18 15:05:31 -0700

    Created 1-alphabet file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.c"
+
+/**
+ * print_alphabet - make alphabets
+ * Return: void
+ */
+void print_alphabet(void);
+{
+	char c;
+
+	for (c = 'a', c <= 'z', c++)
+
+	{
+		_putchar(c);
+	}
+
+	{
+		_putchar("\n");
+	}

6e9a68fd664387a623cefe880bc4a53680e4b2ef
Ofentse Loeto <ohentseloeto610@gmail.com>
2022-10-18 11:47:34 -0700

    Crated a putchar file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,21 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - check your code
+ * main - Prints -putchar to stdout
  *
  * Return: Always 0.
  */
-int main (void)
+int main(void)
 {
-	print("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
 	return (0);
 }

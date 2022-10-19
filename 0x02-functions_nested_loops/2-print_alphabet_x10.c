a10653e417438f330f9bbde73da3e97072930d8f
Ofentse Loeto <ohentseloeto610@gmail.com>
2022-10-19 05:30:29 -0700

    Created task 3

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet x10
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+
+	int = i;
+
+	while (i <= 9)
+	{
+	for (c = 'a'; c <= 'z'; c++)
+
+	{
+	_putchar(c);
+	}
+
+	putchar('\n');
+
+	}
+
+}

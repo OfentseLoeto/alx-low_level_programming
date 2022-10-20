8b6835d1c051ae3318a90f162f44e57b8bd1a0b2
Ofentse Loeto <ohentseloeto610@gmail.com>
2022-10-19 17:33:26 -0700

    Created task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - add two integers and return the results
+ *
+ * @n1: The first one to be computed
+ * @n2: The second oneto be computed
+ *
+ * Return: The integer value
+ */
+
+int add(int n1, int n2)
+{
+	{
+		return (n1 + n2);
+	}
+}

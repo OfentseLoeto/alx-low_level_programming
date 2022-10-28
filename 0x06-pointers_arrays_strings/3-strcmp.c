99c7b5c3a66b95cd873fa6f4f93125c0d185e6a1
Ofentse Loeto <ohentseloeto610@gmail.com>
2022-10-28 12:50:24 -0700

    Created task 2

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compare two string
+ * @s1: String one
+ * @s2 String two
+ *
+ * Return: String value
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int a = 0, b = 0, c = 0, r = 0, lim;
+	char s1;
+	char s2;
+
+	while (s1[a])
+	{
+	a++;
+	}
+
+	while (s2[b])
+	{
+	b++;
+	}
+
+	if (a <= b)
+	{
+	lim = a;
+	}
+
+
+	while (c <= lim)
+	{
+	if (s1[c] == s2[c])
+	{
+	c++;
+	continue;
+	}
+
+	else
+	{
+	r = s1[c] - s2[c];
+	break;
+	}
+
+	c++;
+	}
+	return (r);
+}

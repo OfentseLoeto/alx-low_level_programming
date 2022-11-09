15798896a6c2b34120daf165a8b9f310da44c4c2
Ofentse Loeto <ohentseloeto610@gmail.com>
2022-11-09 12:14:43 -0800

    Created task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the number of args
+ * @argc: Arguments count
+ * @argv: Argument vector
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
+
+

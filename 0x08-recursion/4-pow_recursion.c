e154578cd2d8ced39cae890b43186bae1ff727a4
Ofentse Loeto <ohentseloeto610@gmail.com>
2022-11-23 08:07:26 -0800

    Created task 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,6 +1,26 @@
 #include "main.h"
 
 /**
- * _pow_recursion - returns the value of x raised to the power of y
+ * _pow_recursion - Entry point
+ * @x: input one
+ * @y: input two
  *
+ *
+ * Return: Always success
  */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+
+	return (-1);
+
+	if (y == 0)
+
+	return (1);
+
+	else
+
+	return (x * _pow_recursion(x, y - 1));
+}
+

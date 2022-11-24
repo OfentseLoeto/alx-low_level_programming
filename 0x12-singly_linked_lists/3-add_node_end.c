db7a8e3b79bc6e8d7a200a8205c53fa23092269c
Ofentse Loeto <ohentseloeto610@gmail.com>
2022-11-24 15:51:38 -0800

    Created task 3

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,74 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node_end -  adds a new node at the end of a list_t list
+ * @head: The orginal linked list
+ * @str: The string to add to the node
+ *
+ * Return: The address of the new list or null if it fais
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_list, *temp;
+
+	if (str != NULL)
+
+	{
+
+	new_list = malloc(sizeof(list_t));
+
+	if (new_list == NULL)
+	return (NULL);
+
+	new_list->str = strdup(str);
+	new_list->len = strlen(str);
+	new_list->next = (NULL);
+
+	if (*head == NULL)
+	{
+
+	*head = new_list;
+	return (*head);
+	}
+
+	else
+	{
+
+	temp = *head;
+
+	while
+
+	(temp->next)
+	temp = temp->next;
+	temp->next = new_list;
+	return (temp);
+	}
+	}
+
+	return (NULL);
+}
+
+/**
+ * _strlen - Returns the length of a string
+ * @s: String to count
+ *
+ * Return: The length of a string
+ */
+
+int _strlen(const char *s)
+{
+	int c = 0;
+
+	while (*s)
+
+	{
+	s++;
+	c++;
+	}
+
+	return (c);
+}

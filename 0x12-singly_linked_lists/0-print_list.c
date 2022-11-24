d5473a94f50125442e2000c2aef90e8187ad5c6b
Ofentse Loeto <ohentseloeto610@gmail.com>
2022-11-24 13:03:31 -0800

    Created task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,30 +12,23 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+	size_t nodes = 0;
 
-	if (h == NULL)
-	
-	return (0);
-
-	while (h != NULL)
+	while (h)
 	{
 
 	if (h->str == NULL)
-	{
 
-	printf("[%d] %s\n", 0, "(nil)");
-	}
+	printf("[0] (nil)\n");
 
 	else
-	{
 
 	printf("[%d] %s\n", h->len, h->str);
-	}
+
+	nodes++;
 
 	h = h->next;
-	count++;
 	}
 
-	return (count);
+	return (nodes);
 }

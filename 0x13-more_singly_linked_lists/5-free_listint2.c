349d59f7cf5d21f0a7ae3d4996da2e1b5604f475
OhentseLoeto <ohentseloeto610@gmail.com>
2022-11-29 12:06:45 -0800

    Free list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,6 +1,5 @@
 #include "lists.h"
 #include <stdio.h>
-
 /**
  * free_listint2 - Free listint_t list
  * @head: Head of a linked list
@@ -12,12 +11,12 @@ void free_listint2(listint_t **head)
 {
 
 	listint_t *temp;
+
 	if (head)
 
 	{
 
 	while (*head)
-
 	{
 
 	temp = (*head);
@@ -27,11 +26,12 @@ void free_listint2(listint_t **head)
 	}
 
 	else
-
 	{
 	return;
 	}
-
 	free(*head);
+
+
 	head = 0;
 }
+

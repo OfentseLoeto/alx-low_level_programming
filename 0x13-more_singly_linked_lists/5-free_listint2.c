0ba7106c1a863eb1a131d5a19ef653f266c9c187
OhentseLoeto <ohentseloeto610@gmail.com>
2022-11-29 11:18:00 -0800

    Free a list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * free_listint2 - Free listint_t list
+ * @head: Head of a linked list
+ *
+ * Return: Nothing
+ */
+
+void free_listint2(listint_t **head)
+{
+
+	listint_t *temp;
+	if (head)
+
+	{
+
+	while (*head)
+
+	{
+
+	temp = (*head);
+	*head = (*head)->next;
+	free(temp);
+	}
+	}
+
+	else
+
+	{
+	return;
+	}
+
+	free(*head);
+	head = 0;
+}

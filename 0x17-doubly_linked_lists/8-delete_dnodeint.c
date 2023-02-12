7fea017df4f84191709a176fa1ff629f1f8d6e57
OfentseLoeto <ohentseloeto610@gmail.com>
2023-02-11 23:30:31 -0800

    deletes the node at index

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -19,32 +19,25 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 	while (current_node != NULL)
 	{
-
 	if (index == 0)
 	{
-
 	node_to_delete = current_node;
 	next_node = current_node->next;
 	}
-
 	if (next_node != NULL)
 	{
-
 	next_node->prev = NULL;
 	*head = next_node;
 	free(node_to_delete);
 	return (1);
 	}
-
 	if (i == index)
 	{
-
 	dlistint_t *prev_node = NULL;
 
 	node_to_delete = current_node;
 	next_node = current_node->next;
 	prev_node = current_node->prev;
-
 	if (next_node != NULL)
 
 	next_node->prev = prev_node;
@@ -53,11 +46,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	free(node_to_delete);
 	return (1);
 	}
-
-
 	current_node = current_node->next;
 	i++;
 	}
-
 	return (-1);
 }

d650555c34e1a8d1efc96de86a61a868d5a829b9
OfentseLoeto <ohentseloeto610@gmail.com>
2023-02-16 02:49:54 -0800

    print a hash table

--- 0x1A-hash_tables/5-main.c
+++ 0x1A-hash_tables/5-main.c
@@ -14,6 +14,7 @@ int main(void)
 	hash_table_t *ht;
 
 	ht = hash_table_create(1024);
+	hash_table_print(ht);
 	hash_table_set(ht, "c", "fun");
 	hash_table_set(ht, "python", "awesome");
 	hash_table_set(ht, "Bob", "and Kris loves asm");

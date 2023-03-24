ada8c5f125206bf8374ed6d53679dc99d73857d6
Ofentse Loeto <ohentseloeto610@gmail.com>
2023-03-23 23:00:31 -0700

    Created a grid

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -6,15 +6,17 @@
 def island_perimeter(grid):
 
     """returns the perimeter of the island described in grid"""
-
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
     size = 0
 
-    for i in range(len(grid)):
-            for j in range(len(grid[i])):
-                if grid[i][j]:
-                    size += 4
-                    if i > 0 and grid[i - 1][j] == 1:
-                        size -= 2
-                        if j > 0 and grid[i][j - 1] == 1:
-                            size -= 2
-                            return size
+    for i in range(height):
+            for j in range(width):
+                if grid[i][j] == 1:
+                    size += 1
+                    if j > 0 and grid[j - 1][i] == 1:
+                        edge += 1
+                        if i > 0 and grid[i - 1][j] == 1:
+                            edge += 1
+                            return (size * 4 - edge * 2)

0c91ee3e08a3c76c233471c7e83ed9babcb9070e
Ofentse Loeto <ohentseloeto610@gmail.com>
2023-07-13 00:43:17 -0700

    searches for a value in an array of integers using the Linear search algorithm

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -17,13 +17,11 @@ return (-1);
 
 for (i = 0; i < size; i++)
 {
-	printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+	printf("Value checked array: [%lu] = [%d]\n", i, array[i]);
 
-if (array[i] == value){
-	printf("Found at index: %lu\n", i);
+if (array[i] == value)
 
-return ([i]);
-}
+return (i);
 }
 return (-1);
 
